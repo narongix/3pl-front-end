b519a39946bf9795c7036e89e3c33fe80ccfefd3
Rithiya Vibol <vibolrithiya@gmail.com>
2023-02-15 10:15:21 +0700

    Changes search pattern to START WITH instead of CONTAIN.

--- src/pages/transfers/components/BaseFormField.vue
+++ src/pages/transfers/components/BaseFormField.vue
@@ -229,7 +229,7 @@
                 state:false,
 
                 filters:{
-                    product_name: {operator: FilterOperator.AND, constraints:[{value: null, matchMode: FilterMatchMode.CONTAINS}]},
+                    product_name: {operator: FilterOperator.AND, constraints:[{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
                     demand: {operator: FilterOperator.AND, constraints:[{value: null, matchMode: FilterMatchMode.STARTS_WITH}]}
                 },
                 messageDeletePrompt:{
