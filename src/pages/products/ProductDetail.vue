998ead359aaddb001a427a014f84746fb34ea946
Rithiya Vibol <vibolrithiya@gmail.com>
2023-03-24 09:29:51 +0700

    Add Product Id after product Detail.

--- src/pages/products/ProductDetail.vue
+++ src/pages/products/ProductDetail.vue
@@ -2,7 +2,7 @@
     <div class="grid">
         <div class="col-12">
             <div class="card">
-                <h5>Product Details</h5>
+                <h5>Product Details/{{ $route.params.id }}</h5>
                 <Button :label="buttonLabel" class="p-button-success mr-2" @click="changeEditMode" />
                 <Button v-if="!editDisabled" label="Discard" class="p-button-secondary" @click="discard"></Button>
                 <hr>
