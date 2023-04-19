8935ba31de84fc0ae606bd4923717a193ba9b301
Rithiya Vibol <vibolrithiya@gmail.com>
2023-04-19 14:35:29 +0700

    Add Comma to Decimal if number exceed hundreds.

--- src/components/utils/StringFunction.js
+++ src/components/utils/StringFunction.js
@@ -2,5 +2,10 @@ export default{
     capitalize(text){
         const capitalizeText = text?.charAt(0)?.toUpperCase() + text?.slice(1)       
         return capitalizeText
+    },
+
+    setCharAt(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substring(0,index) + chr + str.substring(index+1);
     }
 }
